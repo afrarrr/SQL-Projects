b3cbee91b04390b21b6ed3134ee37ae30358ec17
afrarrr <86077470+afrarrr@users.noreply.github.com>
2022-02-28 16:52:43 -0800

    Update assignment.sql

--- assignment.sql
+++ assignment.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:396c0929d5c604d20b99adabe8b1ba0800c1e9c77aa1cf4dddab148d6a1caec2
-size 9030
+oid sha256:92e41cb30626468ac0fb6b8bf2c6f84075be4c832ebfef851cf51b2b253612a9
+size 9547
